92e935dd0c7e2a255eab9b90eb7fb98996e3bd81
Chad Estioco <chadestioco@gmail.com>
2020-11-07 13:56:15 +0800

    Savepoint of an approach that I will most probably abandon come morning.
    Really just use Phaser rectangles instead: https://phaser.io/examples/v3/view/game-objects/shapes/rectangle

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
 import Phaser from 'phaser';
+import {SignificantLocation, ExcurzoneGame} from './model';
+
+function gid(id: string): HTMLElement | null {
+    return document.getElementById(id);
+}
+
+function newNode(tag: string): HTMLElement {
+    return document.createElement(tag);
+}
+
+class ExcurzoneController {
+    private targetBoard: HTMLElement | null;
+    constructor(
+        private gameModel: ExcurzoneGame
+    ) {
+        this.targetBoard = gid("excurzone-target");
+        if (this.targetBoard != null){
+            for (var i = 0; i < 40; i++){
+                this.targetBoard.appendChild(newNode("div"));
+            }
+        }
+    }
+}
+
+window.onload = () => {
+    new ExcurzoneController(new ExcurzoneGame([]));
+};
