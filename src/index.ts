7fb0308462caa71ec1da5be45e9d44d5b9b45830
Chad Estioco <chadestioco@gmail.com>
2020-11-07 15:57:14 +0100

    Responsive-ish phaser canvas.
    Why is there no documentation for rendering phaser _in_ an html element?
    I feel like the setup we have right now will bite us but that is much
    much later.

--- src/index.ts
+++ src/index.ts
@@ -9,20 +9,43 @@ function newNode(tag: string): HTMLElement {
     return document.createElement(tag);
 }
 
-class ExcurzoneController {
-    private targetBoard: HTMLElement | null;
-    constructor(
-        private gameModel: ExcurzoneGame
-    ) {
-        this.targetBoard = gid("excurzone-target");
-        if (this.targetBoard != null){
-            for (var i = 0; i < 40; i++){
-                this.targetBoard.appendChild(newNode("div"));
+function configMaker(customKeys: {[index: string]: any} ): Phaser.Types.Core.GameConfig {
+    const config: Phaser.Types.Core.GameConfig = {
+        type: Phaser.AUTO,
+        width: 1024,
+        height: 768,
+        physics: {
+            default: "arcade",
+            arcade: {
+                gravity: {y: 200}
             }
         }
     }
+
+    for (const key of Object.keys(customKeys)) {
+        // @ts-ignore
+        config[key] = customKeys[key];
+    }
+
+    return config;
 }
 
-window.onload = () => {
-    new ExcurzoneController(new ExcurzoneGame([]));
-};
+class ExcurzoneMain extends Phaser.Scene {
+    constructor(
+        private gameModel: ExcurzoneGame
+    ) {
+        super(configMaker({key: "main"}));
+    }
+}
+
+export const game: Phaser.Game;
+
+window.onload = (event) => {
+    const scenesConfig = {"scenes": [ExcurzoneMain]};
+    game = new Phaser.Game(configMaker(scenesConfig));
+    game.scale.resize(window.innerWidth, window.innerHeight);
+}
+
+window.onresize = (event) => {
+    game.scale.resize(window.innerWidth, window.innerHeight);
+});
